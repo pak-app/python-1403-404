66040343b027237d8e277a5aa988297a407509d1
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-04-21 15:37:20 +0330

    complete session 05

---
+++ session-05/test.py
@@ -0,0 +1,33 @@
+# aa
+def encryption(message: str, shift: int=3):
+    
+    '''
+    ## shift
+        **Type**: int
+        *Range*: 1 -> 100
+    '''
+    
+    alphabets = 'abcde'
+    encrypted_message = ''
+    
+    for letter in message:
+        
+        if letter == ' ':
+            encrypted_message += ' '
+            continue
+        
+        current_letter_index = alphabets.index(letter)
+        encrypted_letter_index = current_letter_index + (shift % len(alphabets))
+        encrypted_message += alphabets[encrypted_letter_index]
+        shift += 1
+    
+    return encrypted_message
+
+real_msg = 'add eac'
+shift = 3
+print(encryption.__doc__)
+print('Real message:', real_msg)
+# print('Encrypted message:', encryption(real_msg))
+
+        
+    
